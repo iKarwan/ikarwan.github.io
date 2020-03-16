e535db89763a9fc37646ef322adce9aaf38d2d74
Karwan-Ghaffari <karwan-ghafari@hotmail.com>
2020-03-16 23:15:03 +0300

    .

--- description.js
+++ description.js
@@ -23,7 +23,7 @@ function toogleTheme()
 $(function() {
   $("li").on("click",function() {
 	  if(this.id=="dnt") {
-		  $("#dnt_txt").html("You can donate USD via PayPal mail: julioverne"+"@"+"icloud.com");
+		  $("#dnt_txt").html("You can donate USD via PayPal mail: karwan.office"+"@"+"bk.ru");
 	  }
   });
 });
@@ -32,7 +32,7 @@ function loadPackageInfo() {
 	var form_url = urlSelfParts[0]+"packageInfo/"+urlSelfParts[1];
 	if (navigator.userAgent.search(/Cydia/) == -1) {
 		$("#showAddRepo_").show();
-		$("#open_cydia").attr("href","cydia://url/https://cydia.saurik.com/api/share#?source=https://julioverne.github.io/&package="+urlSelfParts[1]);
+		$("#open_cydia").attr("href","cydia://url/https://cydia.saurik.com/api/share#?source=https://ikarwan.github.io/&package="+urlSelfParts[1]);
 	}
 	$.ajax({
 		url: form_url,
