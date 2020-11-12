33850d50199e1105ba5b5807c532553e77c7e9e5
Karwan-Ghaffari <karwan-ghafari@hotmail.com>
2020-11-12 08:48:50 +0300

    .

---
+++ UDID-master/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+            <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Page Title</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/navbar.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/footer.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/register.css" media="screen" title="no title">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="row" style="
+    margin-right: 15px;
+    margin-left: 15px;
+">
+    <div class="col-sm-12" id="logout">
+        <div class="page-header">
+            <h3 class="reviews" style="
+    text-align: center;
+">مرحباً بك </h3>
+         
+        </div>
+        <div class="comment-tabs">
+             
+            <div class="tab-content">
+                <div class="tab-pane active" id="comments-logout">                
+                    <ul class="media-list">
+                      <li class="media pull-right">
+
+                        <div class="media-body">
+                          <div class="well well-lg">
+                             
+                              <p class="arrow-alt-circle-left" style="
+    text-align: center;
+">
+                              لمعرفة رقم اليودي آي دي ، الرجاء
+                              </p>
+                              <br>
+                              <center>
+                              <a class="btn btn-info btn-circle text-uppercase" href="udid.php" id="reply" style="
+    text-align:  center;
+    color: white;
+    font-weight: bold;
+    margin-top: -12px;
+    background: linear-gradient(135deg, #95e690 0%,#45a2b1 100%);
+    border-radius: 66px;
+    display: inline-block;
+    padding: 3.9px 29px;
+    border-color: #46b8da00;
+">
+                                  <span class="arrow-alt-circle-left" ></span> آضغط هنا</a> </center>
+                             
+                          </div>              
+                        </div>
+     
+                      </li>          
+                     </ul>  
+                      
+       
+               
+                </div>  
+         </div>
+
+            </div>
+        
+        </div>
+	</div>
+
+</body>
+</html>
