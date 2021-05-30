4245707f3e6351dda322531798c90719b6747584
iKarwan <karwan-ghafari@hotmail.com>
2021-05-30 17:19:53 +0300

    Update submit.php

--- Get-iOS-UDID-master/complete/submit.php
+++ Get-iOS-UDID-master/complete/submit.php
@@ -1,6 +1,5 @@
 <?php
 	$a = $_POST['udid'] . "\n" . $_POST['name'] . "\n" . $_POST['email'] . "\n" . $_POST['product'];
-	mail("YOU@YOUR-DOMAIN.c-o-m", "UDID", $a);
+	mail("ikg_hd@bk.ru", "UDID", $a);
 	header('Location: finished.html');
 ?>
-	
